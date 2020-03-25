21baf651e37b94be3dc405dcabf1ad5446f392fa
Dinh Quang <quangddinh@gmail.com>
2020-03-26 01:07:25 +0700

    lib

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // b1: create request, class chứa tất cả request của use
+        // có bnhiu request thì đưa hết vào 1 cái interface
+        // mỗi lần muốn sử dụng tạo 1 cái retrofit
+        // sau đó truyền đường dẫn tới server (end point)
     }
 }
